6d86fab6f8393b1bed62245935104fdaffacd136
TahaHanif2424 <tahahanif24@gmail.com>
2026-01-15 00:42:57 +0500

    All icons in signup page and login page move to separate file

--- src/components/c-level/index.ts
+++ src/components/c-level/index.ts
@@ -1,2 +1,13 @@
 export { default as Button } from './Button';
 export { default as Input } from './Input';
+export {
+  UserIcon,
+  EmailIcon,
+  LockIcon,
+  EyeIcon,
+  EyeOffIcon,
+  GoogleIcon,
+  FacebookIcon,
+  AppleIcon,
+  PasswordToggle,
+} from './Icons';
